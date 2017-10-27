bd305135c3042c53fc7fe9aad44bc3afbde8e39e
vladfreel <vladfreelmaster@gmail.com>
2017-10-27 16:11:55 +0300

    finish with admin panel

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  def find
-    User.find()
-  end
 end
