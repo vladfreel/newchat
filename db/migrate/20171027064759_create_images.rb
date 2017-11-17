16efd7c6276086f7eb801919f89d19c31de2f714
vladfreel <vladfreelmaster@gmail.com>
2017-11-17 09:07:50 +0200

    migrate likes

--- db/migrate/20171027064759_create_images.rb
+++ db/migrate/20171027064759_create_images.rb
@@ -2,7 +2,6 @@ class CreateImages < ActiveRecord::Migration[5.1]
   def change
     create_table :images do |t|
       t.string :path
-      t.integer :likes
       t.references :category, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113122845) do
+ActiveRecord::Schema.define(version: 20171117070603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,8 +88,6 @@ ActiveRecord::Schema.define(version: 20171113122845) do
   create_table "likes", force: :cascade do |t|
     t.bigint "image_id"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["image_id"], name: "index_likes_on_image_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
