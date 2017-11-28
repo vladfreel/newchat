8678f64ed93e35eb5b898c7b604e2376c2a14d0b
vladfreel <vladfreelmaster@gmail.com>
2017-11-28 15:13:31 +0200

    some changes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user.persisted?
       sign_in_and_redirect user, notice: 'Signed in!!!'
+      action_t = 'Sign in'
+      event_create(action_t)
     else
       session['devise.user_attributes'] = user.attributes
       redirect_to new_user_registration_url
